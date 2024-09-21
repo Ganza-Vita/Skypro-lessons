5aa7e1f886ad0694b2d740970c92d6e19ac7a973
Bulat Galimov <ganza.alliance@gmail.com>
2024-09-21 19:10:06 +0300

    created_a_function

--- src/widget.py
+++ src/widget.py
@@ -1,1 +1,10 @@
-from masks import
+from masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(card_number: str) -> str:
+    """ Функция маскирующая номер карты и номер счета """
+    if card_number != 16:
+        get_mask_account(card_number)
+    else:
+        get_mask_card_number(card_number)
+
