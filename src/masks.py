5209f50bce57fdc6e78e286f8c1c42b70dbebcb7
Bulat Galimov <ganza.alliance@gmail.com>
2024-09-21 10:24:39 +0300

    src/masks.py

---
+++ src/masks.py
@@ -0,0 +1,20 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция маскирующая номер карты"""
+    if len(card_number) != 16 or not card_number.isdigit():
+        raise ValueError("Номер карты должен содержать 16 цифр.")
+
+    masked_number = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+
+    return masked_number
+
+
+def get_mask_account(account_number: str | int) -> str | int:
+    """Функция маскирующая номер счета"""
+
+    account_number = str(account_number)
+
+    if len(account_number) < 4:
+        return "Номер счета должен содержать как минимум 4 цифры."
+
+    mask = f"**{account_number[-4:]}"
+    return mask
