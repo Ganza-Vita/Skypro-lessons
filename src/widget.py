efdcafdb552f215f49adc0a5f15f87826f96337d
Bulat Galimov <ganza.alliance@gmail.com>
2024-09-21 19:26:27 +0300

    finally_stable_version

--- src/widget.py
+++ src/widget.py
@@ -1,10 +1,15 @@
-from masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_card_number, get_mask_account
 
 
 def mask_account_card(card_number: str) -> str:
     """ Функция маскирующая номер карты и номер счета """
-    if card_number != 16:
-        get_mask_account(card_number)
+    formatted_number = ""
+    for number in card_number:
+        if number.isdigit():
+            formatted_number += number
+    if len(formatted_number) != 16:
+        return get_mask_account(formatted_number)
     else:
-        get_mask_card_number(card_number)
+        return get_mask_card_number(formatted_number)
+    print(formatted_number)
 
