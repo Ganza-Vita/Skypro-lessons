0bf6d4e7e52a1d21f2f69bf4445cab512cbd61fa
Bulat Galimov <ganza.alliance@gmail.com>
2024-09-21 19:42:32 +0300

    finally

--- src/widget.py
+++ src/widget.py
@@ -1,14 +1,25 @@
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(card_number: str) -> str:
-    """ Функция маскирующая номер карты и номер счета """
+    """Функция маскирующая номер карты и номер счета"""
     formatted_number = ""
+
     for number in card_number:
         if number.isdigit():
             formatted_number += number
-    if len(formatted_number) != 16:
-        return get_mask_account(formatted_number)
-    else:
-        return get_mask_card_number(formatted_number)
 
+    if len(formatted_number) != 16:
+        return str(get_mask_account(formatted_number))
+    else:
+        return str(get_mask_card_number(formatted_number))
+
+
+def get_date(date_string: str) -> str:
+    date_part = date_string.split("T")[0]
+
+    year, month, day = date_part.split("-")
+
+    formatted_date = f"{day}.{month}.{year}"
+
+    return formatted_date
