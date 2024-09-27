f81ebe1407f4d2f981f62ea4466b72b84f2fd05b
Bulat Galimov <ganza.alliance@gmail.com>
2024-09-28 00:18:38 +0300

    Bugs fixed

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
 from typing import List, Dict
 
 
-def filter_by_state(operations: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
+def filter_by_state(operations: List[Dict], state: str = "EXECUTED") -> List[Dict]:
     """
     Функция фильтрует операции по состоянию.
     """
@@ -9,7 +9,7 @@ def filter_by_state(operations: List[Dict], state: str = 'EXECUTED') -> List[Dic
     filtered_operations = []
 
     for operation in operations:
-        if operation.get('state') == state:
+        if operation.get("state") == state:
             filtered_operations.append(operation)
 
     return filtered_operations
@@ -20,6 +20,7 @@ def sort_by_date(operations: List[Dict], reverse: bool = True) -> List[Dict]:
     Функция сортирует операции по дате.
     """
 
-    sorted_operations = sorted(operations, key=lambda operation: operation.get('date'), reverse=reverse)
+    sorted_operations = sorted(operations, key=lambda operation: operation.get("date", "Дата не введена"),
+                               reverse=reverse)
 
     return sorted_operations
