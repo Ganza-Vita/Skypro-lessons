ab708be83a2cdb8480c893e824843f28c0f01835
Bulat Galimov <ganza.alliance@gmail.com>
2024-09-27 21:04:21 +0300

    give finction "filter_by_state"

--- src/processing.py
+++ src/processing.py
@@ -3,5 +3,13 @@ from typing import List, Dict
 
 def filter_by_state(operations: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
     """
-    Функция фильтрует операции по состоянию
+    Функция фильтрует операции по состоянию.
     """
+
+    filtered_operations = []
+
+    for operation in operations:
+        if operation.get('state') == state:
+            filtered_operations.append(operation)
+
+    return filtered_operations
