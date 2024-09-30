348c68c916adfda1a32f6341295be92985f9189b
Bulat Galimov <ganza.alliance@gmail.com>
2024-09-30 23:14:34 +0300

    widget.py test coverage 100%

--- src/widget.py
+++ src/widget.py
@@ -1,20 +1,27 @@
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(card_number: str) -> str:
+def mask_account_card(card_or_account_number: str) -> str:
     """Функция маскирующая номер карты и номер счета"""
     formatted_number = ""
-    formatted_letter = ""
-    for number in card_number:
-        if number.isdigit():
-            formatted_number += number
-        if number.isalpha():
-            formatted_letter += number
+    formatted_label = ""
 
-    if len(formatted_number) != 16:
-        return str(f"{formatted_letter} {get_mask_account(formatted_number)}")
+    for char in card_or_account_number:
+        if char.isdigit():
+            formatted_number += char
+        elif char.isalpha() or char.isspace():
+            formatted_label += char
+
+    formatted_label = " ".join(formatted_label.split())
+
+    if len(formatted_number) == 16:
+        return f"{formatted_label} {formatted_number[:4]} {formatted_number[4:6]}** **** {formatted_number[12:]}"
+
+    elif len(formatted_number) >= 20:
+        return f"{formatted_label} **{formatted_number[-4:]}"
+
     else:
-        return str(f"{formatted_letter} {get_mask_card_number(formatted_number)}")
+        return "Некорректный номер"
 
 
 def get_date(date_string: str) -> str:
